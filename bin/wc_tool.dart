7ff53098dc901840a6ac3e64175bc8d4d9b239c8
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-08-24 23:56:24 -0700

    Able to read the number of bytes in the file

--- bin/wc_tool.dart
+++ bin/wc_tool.dart
@@ -1,3 +1,20 @@
-void main() {
-  print("hello wc");
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:typed_data';
+
+void main(List<String> args) {
+  // step 1: print number of bytes in the file
+  String? command;
+  if (args.isNotEmpty) {
+    command = args[0];
+    File config = File("files/test.txt");
+    switch (command) {
+      case "-c":
+        // the task for -c is to print the number of bytes in the file
+        List<int> bytes = config.readAsBytesSync();
+        print("number of bytes: ${bytes.length}");
+    }
+  } else {
+    print("Please enter an appropriate command");
+  }
 }
